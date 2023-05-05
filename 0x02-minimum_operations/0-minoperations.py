ccab201adb9d723d00e4a93e227a169ff4b7af0f
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-05-05 15:00:08 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
+"""
+
+
+def minOperations(n):
     if n < 2:
         return 0
-    factor_list = []
+    list_factor = []
     i = 1
     while n != 1:
         i += 1
         if n % i == 0:
             while n % i == 0:
                 n /= i
-                factor_list.append(i)
-    return sum(factor_list)
+                list_factor.append(i)
+    return sum(list_factor)
