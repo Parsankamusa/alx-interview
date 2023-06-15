002d139e424f3a92d9f02b21529252a61390ff9d
Parsanka <91263172+Parsankamusa@users.noreply.github.com>
2023-06-16 01:18:04 +0300

    making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,15 +21,15 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     try:
-        Min = [float('inf') for i in range(total+1)]
-        Min[0] = 0
+        lowest = [float('inf') for i in range(total+1)]
+        lowest[0] = 0
         for i in range(1, total+1):
             for j in range(len(coins)):
-                if Min[i - coins[j]] + 1 < Min[i]:
-                    Min[i] = Min[i - coins[j]] + 1
+                if lowest[i - coins[j]] + 1 < lowest[i]:
+                    lowest[i] = lowest[i - coins[j]] + 1
 
-        if Min[total] != float('inf'):
-            return Min[total]
+        if lowest[total] != float('inf'):
+            return lowest[total]
         else:
             return -1
     except Exception:
